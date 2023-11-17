cf97ad86f9c736525d26150115e17e8545d03222
Bird-Forest <bird.forest70@gmail.com>
2023-11-17 15:17:53 +0200

    add file

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,39 @@
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.random();
+    }
+    getSignature() {
+        return this.signature;
+    }
+}
+class Person {
+    constructor(private key: Key) { }
+    getKey() {
+    return this.key;
+    }
+}
+abstract class House {
+    door: boolean;
+    key: Key;
+    tenants: Person[];
+    
+    comeIn(person: Person) {
+        if (this.door)  
+            this.tenants.push(person); 
+    }
+    abstract openDoor(key: Key): boolean;
+}
+class MyHouse extends House {
+    constructor(key: Key) {
+        super();
+    }
+    openDoor(key: Key) {
+        if (this.key.getSignature() === key.getSignature()) return this.door = true;
+        else  return this.door = false;   
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
